113b4c0a49923e7fe8baef91e14d80a73869a121
Emre_Turkan <emre_turkan_98@hotmail.com>
2022-06-07 21:27:47 +0300

    first commit

---
+++ src/components/Pokeinfo.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const PokeInfo=()=>{
+
+return(
+    <>
+    <h1>Charmander</h1>
+    <img src="" alt=""/>
+    <div className="abilities">
+    <div className="group">
+        <h2>blaze</h2>
+        </div>
+        <div className="group">
+        <h2>solar-power</h2>
+       </div>
+       </div>
+       <div className="base-start">
+           <h3>hp:30</h3>
+           <h3>attack:52</h3>
+           <h3>defense:43</h3>
+           <h3>speacial-attack</h3>
+           <h3>Speed</h3>
+       </div>
+      
+    </>
+);
+};
+
+export default PokeInfo;
